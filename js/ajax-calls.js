ff87eae5501555e4f0f3e56e20efb0cdeec2be80
Alex McCracken <alexmccracken55@gmail.com>
2016-07-13 17:29:55 -0400

    Finished up first pass of the Bubble Sort application  ready for polishing/additional documentation/testing

--- js/ajax-calls.js
+++ js/ajax-calls.js
@@ -1,20 +1,27 @@
 /**
- * Generic all purpose call to index.php
- * Handles Shuffle, Step, and Play. If anything else is passed in the 'else'
- * statement will take over and just return resulting in nothing occurring.
+ * ajax-calls.js handles making the AJAX calls to 'index.php'
+ * Whenever the 'shuffle/step/play buttons are pushed it will relay that to the server
+ * Which will then handle generating the necessary data.
+ * 
  * Created by Alex on 7/13/2016.
  */
 
+/**
+ * This function is in charge of determining if we are done with the sort.
+ * As well as enabling the Step/Play buttons and making the AJAX call the appropriate amount of times
+ *
+ * @param operation -> The operation that we are passing into 'index.php'
+ */
 function ajaxCall(operation) {
     var done_yet = false; // Testing variable to see if we are done sorting
-    //Enable elements since initialized has been pressed if we are here
     document.getElementById("step").disabled = false;
     document.getElementById("play").disabled = false;
 
-    //If 'play' was pressed run the algorithm to completion else run one step
+    //If 'play' was pressed run the algorithm to completion
     if (operation == "play") {
         for(var x=0; x<100; x++){
-            done_yet = setTimeout(bubbleSortCall("step"), 10000000);
+            //TODO: setTimeout() is not working come up with another way to slow it down a little
+            done_yet = setTimeout(bubbleSortCall("step"), 0);
             if(done_yet == true){
                 break;
             }
@@ -25,6 +32,14 @@ function ajaxCall(operation) {
     }
 }
 
+/**
+ * Calls the Bubble Sort Algorithm
+ *
+ * Result will either be an HTML table for us to append to the bubblesort-container DIV or it will return
+ * 'disable' to trigger a 'win/finished condition'
+ *
+ * @param operation -> The operation that we are passing into 'index.php'
+ */
 function bubbleSortCall(operation){
     $.ajax({
         type: "POST",
