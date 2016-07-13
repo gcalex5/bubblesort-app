d2d4853e0665a317fded7aa5cdc2a8faa6a93620
Alex McCracken <alexmccracken55@gmail.com>
2016-07-13 09:47:51 -0400

    Initial commit with requirements list

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Front page of bubble sort app
+ * User: Alex
+ * Date: 7/13/2016
+ * Time: 9:28 AM
+ */
+
+//Initial requirements list
+//TODO: Application should visually display the algorithm for a vector of 10 integers
+//TODO: Two buttons are required Shuffle and Step
+//TODO: Shuffle -> Initialize array with 10 random integers between 0 and 100
+//TODO: Step -> Step performs one step of the algorithm and displays it on the page
+//TODO: Display array as table with 10 rows
+//TODO: Each row will contain a colored rectangle corresponding to the number
+//TODO: When step is pressed the two numbers being compared should be highlighted with a different color
+//TODO: Continually pressing step with sort the numbers from largest(top of page) to smalled(bottom of page)
+//TODO: When sort is finished hide or disable the button
+//TODO: All processing needs to be done on the server
+//TODO: BONUS: Play button that simulates steps, reloads the table using AJAX calls
+//TODO: BONUS: Drupal module: configurable, dedicated path, etc.
