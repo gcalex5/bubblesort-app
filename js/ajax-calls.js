10792bc87a0c354906bcacdd6a2fcdee6bff0f1f
Alex McCracken <alexmccracken55@gmail.com>
2016-07-13 19:47:05 -0400

    AJAX functioning with 'play' option just fine
    Attempted to slow it down with setTimeout/setInterval, commented out code is functional but appears to be executing in batches, so reverted back to a simple for loop until I have more time to mess with it.

--- js/ajax-calls.js
+++ js/ajax-calls.js
@@ -12,16 +12,18 @@
  *
  * @param operation -> The operation that we are passing into 'index.php'
  */
+//var timeoutVar = 0; //Timeout variable for 'play' option.
+var done_yet = false; //Tester variable to see if we are done sorting.
 function ajaxCall(operation) {
-    var done_yet = false; // Testing variable to see if we are done sorting
     document.getElementById("step").disabled = false;
     document.getElementById("play").disabled = false;
 
     //If 'play' was pressed run the algorithm to completion
     if (operation == "play") {
+        document.getElementById("step").disabled = true;
+        document.getElementById("play").disabled = true;
         for(var x=0; x<100; x++){
-            //TODO: setTimeout() is not working come up with another way to slow it down a little
-            done_yet = bubbleSortCall("step");
+            bubbleSortCall("step");
             if(done_yet == true){
                 break;
             }
@@ -32,6 +34,29 @@ function ajaxCall(operation) {
     }
 }
 
+//TODO: Experiment with this code everything appears to be executing in batches
+/**
+ * Special handler for 'Play' to slow it down a little
+
+function playCalls(){
+    timeoutVar = setTimeout(playCalls, 500);
+    bubbleSortCall("step"); // Testing variable to see if we are done sorting
+    if(done_yet == undefined || done_yet == false){
+        console.log(timeoutVar + " " + done_yet);
+    }
+    else if(done_yet == true){
+        stopPlayCalls();
+    }
+
+}
+
+/**
+ * Stop the infinite loop created with setTimeout()
+
+function stopPlayCalls(){
+    clearTimeout(timeoutVar);
+}
+**/
 /**
  * Calls the Bubble Sort Algorithm
  *
@@ -40,6 +65,8 @@ function ajaxCall(operation) {
  *
  * @param operation -> The operation that we are passing into 'index.php'
  */
+
+
 function bubbleSortCall(operation){
     $.ajax({
         type: "POST",
@@ -54,10 +81,11 @@ function bubbleSortCall(operation){
         if(trimmed_result.toString() == "disable"){
             document.getElementById("step").disabled = true;
             document.getElementById("play").disabled = true;
-            return true;
+            done_yet = true;
         }
         else{
             $(".bubblesort-container").empty().append(trimmed_result);
+            done_yet = false;
         }
     });
 }
